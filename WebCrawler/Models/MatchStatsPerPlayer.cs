80407c52bb1d5705d310ce53327b75edb6954d23
Andrew Yirak <7884352+drewisninja@users.noreply.github.com>
2019-04-12 01:06:21 -0700

    Testing out Commit
    Testing out Commit

---
+++ WebCrawler/Models/MatchStatsPerPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebCrawler.Models
+{
+    public class MatchStatsPerPlayer
+    {
+    }
+}
---
+++ WebCrawler/Program.cs
@@ -0,0 +1,35 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using WebCrawler.BusinessLogic;
+
+namespace WebCrawler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var driver = new ChromeDriver("C:/Users/ayira/Desktop/APA"))
+            {
+                LoginPage LP = new LoginPage(driver, "https://league.poolplayers.com/login");
+                LP.SignIn();
+
+                //PlayerStatsProcessor processor = new PlayerStatsProcessor(driver, "https://league.poolplayers.com/oc/divisions/281050/standings");
+                //processor.ProcessPlayerHighLevelStats();
+
+                PlayerStatsProcessor processor = new PlayerStatsProcessor(driver, "https://league.poolplayers.com/oc/divisions/281050/schedule/");
+                processor.GrabAllMatchHistory();
+
+                var pause = "";
+
+                driver.Close();
+                driver.Quit();
+            }
+        }
+    }
+}
